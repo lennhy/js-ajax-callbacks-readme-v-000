7851b9fe88ce2cd2c3fa8c4c8832e896abfbc2d4
lenn hypolite <lennhydesign@gmail.com>
2016-11-10 23:55:27 -0500

    Done

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // We should wait for the page to load before running our ajax request
 $(document).ready(function(){
+  $.get("this_doesnt_exist.html", function(data) {
+  // This will not be called because the .html file request doesn't exist
+    doSomethingGood();
+  }).fail(function(error) {
+    
+  // This is called when an error occurs
+  console.log('Something went wrong: ' + error);
+});
   // Now we start the Ajax Get request. The first parameter is the URL with the data.
   // The second parameter is a function that handles the response.
   $.get("html/sentence.html", function(response) {
